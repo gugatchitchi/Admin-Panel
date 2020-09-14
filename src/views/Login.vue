800b6dd2bd2b929c232596018497ec66a8a0dc20
Guga <gugatchitchinadze@gmail.com>
2020-09-14 21:02:45 +0400

    all the default code is rewritten

---
+++ src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <h1>Login Page</h1>
+    <router-link to="/">Home Page</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  components: {}
+}
+</script>
